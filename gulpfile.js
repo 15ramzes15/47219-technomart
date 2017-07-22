4f75eca98f3d201e7a90afc31021de0644e7a5b6
Alexey <15ramzes15@gmail.com>
2017-07-22 16:56:27 +0300

    Добавил сборку на Gulp

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var clean = require("gulp-clean");
+var uglify = require("gulp-uglify");
+
+
+gulp.task("default", ["clean"], function() {
+	gulp.run('dev');
+});
+
+gulp.task('dev', ["style", "script", "serve"]);
+
+gulp.task("style", function(){
+  return gulp.src("sass/style.scss")
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+          title: 'Styles',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+      "last 2 versions"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("script", function() {
+  gulp.src("js/main.js")
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+          title: 'Scripts',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(uglify())
+    .pipe(rename("main.min.js"))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: "."
+  }); 
+
+  gulp.watch("sass/style.scss", ["style"]);
+  gulp.watch("js/main.js", ["script"]);
+  gulp.watch("*.html")
+    .on("change", server.reload);
+});
+
+gulp.task("clean", function() {
+	return gulp.src("css/")
+		.pipe(clean());
+})
